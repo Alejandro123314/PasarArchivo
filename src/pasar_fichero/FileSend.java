cc1f87ad59516bb0aacc259f354628765816d1ed
Alejandro123314 <alejandrojimenezclaro@gmail.com>
2023-02-14 09:37:32 +0000

    send

--- src/pasar_fichero/FileSend.java
+++ src/pasar_fichero/FileSend.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 
 import javax.swing.JFileChooser;
 
@@ -32,11 +33,15 @@ public class FileSend {
 				DatagramPacket fileStatPacket = new DatagramPacket(fileNameBytes, fileNameBytes.length, address, port);
 				socket.send(fileStatPacket);
 				byte[] fileByteArray = readFileToByteArray(f);
+				sendFile(socket, fileByteArray, address, port);
 				
 			}
+			
+			socket.close();
 
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
+			System.exit(1);
 		}
 	}
 	
@@ -52,6 +57,49 @@ public class FileSend {
 			byte[] message = new byte[1024];
 			message[0] = (byte) (sequencerNumber >> 8);
 			message[1] = (byte) (sequencerNumber);
+			
+			if((i+2021) >= fileByteArray.length) {
+				flag = true;
+				message[2] = (byte) (1);
+			} else {
+				flag = false;
+				message[2] = (byte) (0);
+			}
+			
+			if(!flag) {
+				System.arraycopy(fileByteArray, i, message, 3,1021);
+			} else {
+				System.arraycopy(fileByteArray, i, message, 3, fileByteArray.length-i);
+			}
+			
+			DatagramPacket sendPacket = new DatagramPacket(message, message.length, address, port);
+			socket.send(sendPacket);
+			System.out.println("Sent: Sequence number = " + sequencerNumber);
+			
+			boolean ackRec;
+			
+			while (true) {
+				byte[] ack = new byte[2];
+				DatagramPacket ackpack = new DatagramPacket(ack, ack.length);
+				
+				try {
+					socket.setSoTimeout(50);
+					socket.receive(ackpack);
+					ackSequence = ((ack[0] & 0xff) << 8) + (ack[1] & 0xff);
+					ackRec = true;
+				} catch (SocketTimeoutException e) {
+					System.out.println("Socket timed out waiting for ack");
+					ackRec = false;
+				}
+				
+				if ((ackSequence == sequencerNumber) && (ackRec)) {
+					System.out.println("Ack received: Sequence Number = " + ackSequence);
+					break;
+				} else {
+					socket.send(ackpack);
+					System.out.println("Resending: Sequence Number = " + sequencerNumber);
+				}
+			}
 		}
 	}
 
